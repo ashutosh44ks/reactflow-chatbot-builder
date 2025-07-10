2816c22534dcd3a0b40fbd5460c51cdc4db2500a
Ashutosh Singh <ashutosh.singh@oodles.io>
2025-07-11 01:24:18 +0530

    refactor: simplify main rendering by removing unnecessary providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { ReactFlowProvider } from "@xyflow/react";
-import { DnDProvider } from "./DnDContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ReactFlowProvider>
-      <DnDProvider>
-        <App />
-      </DnDProvider>
-    </ReactFlowProvider>
+    hello
   </StrictMode>
 );
